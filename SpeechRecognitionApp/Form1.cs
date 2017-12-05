4279b44f07d7194e4d70fb8614ebd736ecfb1163
Coriiu Davd <coriiudavd@gmail.com>
2017-12-05 20:07:45 +0200

    ora pe glob pentru anumite orase puse intr-un dictionar preconfgurat

--- SpeechRecognitionApp/Form1.cs
+++ SpeechRecognitionApp/Form1.cs
@@ -14,22 +14,54 @@ using System.Threading;
 using System.Diagnostics;
 using System.Net;
 using System.Globalization;
-//using Microsoft.Speech;
-//using Microsoft.Speech.Synthesis;
-//using Microsoft.Speech.Recognition;
-
-
 
 namespace SpeechRecognitionApp {
-    
+
     public partial class Form1 : Form {
 
+        Dictionary<string, int> CitiesTimeZonesDicitonary = new Dictionary<string, int>() {
+            {"Bucharest", 0 },
+            {"Dublin", -1 },
+            {"London", -2 },
+            {"Barcelona", -1 },
+            {"Oslo", -1 },
+            {"Paris", -1 },
+            {"New York", -9 },
+            {"Toronto", -10 },
+            {"Moscow", 1 },
+            {"Dubai", 2 },
+            {"Singapore", 6 },
+            {"Honk kong", 6 },
+            {"Melbourne", 9 },
+            {"Tokyo", 7 },
+            {"Jakata", 5 },
+        };
+
         string currentDir = System.AppDomain.CurrentDomain.BaseDirectory;
+
         //Form Declaration
         SpeechSynthesizer ss = new SpeechSynthesizer();
         PromptBuilder pb = new PromptBuilder();
         SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
-        Choices clist = new Choices(new string[] { "hello", "how are you", "what is the current time", "open chrome",
+        Choices clist = new Choices(new string[] { "hello", "how are you",
+                                    "what is the current time",
+                                    "what is the current time in Bucharest",
+                                    "what is the current time in London",
+                                    "what is the current time in New York",
+                                    "what is the current time in Paris",
+                                    "what is the current time in Barcelona",
+                                    "what is the current time in Oslo",
+                                    "what is the current time in Dublin",
+                                    "what is the current time in Toronto",
+                                    "what is the current time in Moscow",
+                                    "what is the current time in Dubai",
+                                    "what is the current time in Singapore",
+                                    "what is the current time in Honk kong",
+                                    "what is the current time in Melbourne",
+                                    "what is the current time in Tokyo",
+                                    "what is the current time in Jakarta",
+                                    "open chrome",
+                                    "open notepad",
                                     "thank you", "close",
                                     "get current date",
                                     "get battery status",
@@ -40,52 +72,103 @@ namespace SpeechRecognitionApp {
                                     "tell currency-exchange", "tell currency-exchange to Dollar"});
 
         public Form1() {
-            
+
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e) {
             //start
-           
-                btnStart.Enabled = false;
-                btnStop.Enabled = true;
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
 
-            
-                //clist.Add(new string[] { "hello", "how are you", "what is the current time", "open chrome",
-                //"thank you", "close", "get current date",
-                //                        "get battery status", "get IP address", "check Internet connection", 
-                //                        "open windows media player", "open paint", "get bitcoin value",
-                //                        "tell currency-exchange", "tell currency-exchange to Dollar"});
+            Grammar gr = new Grammar(new GrammarBuilder(clist));
 
-                Grammar gr = new Grammar(new GrammarBuilder(clist));
+            try {
+                sre.RequestRecognizerUpdate();
+                sre.LoadGrammar(gr);
+                sre.SpeechRecognized += sre_SpeechRecognized;
+                sre.SetInputToDefaultAudioDevice();
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message, "error");
+            }
 
-                try {
-                    sre.RequestRecognizerUpdate();
-                    sre.LoadGrammar(gr);
-                    sre.SpeechRecognized += sre_SpeechRecognized;
-                    sre.SetInputToDefaultAudioDevice();
-                    sre.RecognizeAsync(RecognizeMode.Multiple);
-                } catch (Exception ex) {
-                    MessageBox.Show(ex.Message, "error");
-                }
-            
         }
 
+
         private void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e) {
-            
+            txtContents.Text += e.Result.Text.ToString() + Environment.NewLine;
+
             switch (e.Result.Text.ToString()) {
                 case "hello":
                     ss.SpeakAsync("Hello");
                     break;
                 case "what is the current time":
                     ss.SpeakAsync("current time is " + DateTime.Now.ToLongTimeString());
-                    txtContents.Text += DateTime.Now.ToLongTimeString() + Environment.NewLine; 
+                    txtContents.Text += DateTime.Now.ToLongTimeString() + Environment.NewLine;
                     break;
+                case "what is the current time in Bucharest":
+                    ss.SpeakAsync("current time in Bucharest is " + getDateTime("Bucharest").ToLongTimeString());
+                    txtContents.Text += getDateTime("Bucharest").ToLongTimeString() + Environment.NewLine;
+                    break;
+                case "what is the current time in London":
+                    ss.SpeakAsync("current time in London is " + getDateTime("London").ToLongTimeString());
+                    txtContents.Text += getDateTime("London").ToLongTimeString() + Environment.NewLine;
+                    break;
+                case "what is the current time in Oslo":
+                    ss.SpeakAsync("current time in Oslo is " + getDateTime("Oslo").ToLongTimeString());
+                    txtContents.Text += getDateTime("Oslo").ToLongTimeString() + Environment.NewLine;
+                    break;
+                case "what is the current time in Barcelona":
+                    ss.SpeakAsync("current time in Barcelona is " + getDateTime("Barcelona").ToLongTimeString());
+                    txtContents.Text += getDateTime("Barcelona").ToLongTimeString() + Environment.NewLine;
+                    break;
+                case "what is the current time in New York":
+                    ss.SpeakAsync("current time in New York is " + getDateTime("New York").ToLongTimeString());
+                    txtContents.Text += getDateTime("New York").ToLongTimeString() + Environment.NewLine;
+                    break;
+                case "what is the current time in Toronto":
+                    ss.SpeakAsync("current time in Toronto is " + getDateTime("Toronto").ToLongTimeString());
+                    txtContents.Text += getDateTime("Toronto").ToLongTimeString() + Environment.NewLine;
+                    break;
+                case "what is the current time in Moscow":
+                    ss.SpeakAsync("current time in Moscow is " + getDateTime("Moscow").ToLongTimeString());
+                    txtContents.Text += getDateTime("Moscow").ToLongTimeString() + Environment.NewLine;
+                    break;
+                case "what is the current time in Dubai":
+                    ss.SpeakAsync("current time in Dubai is " + getDateTime("Dubai").ToLongTimeString());
+                    txtContents.Text += getDateTime("Dubai").ToLongTimeString() + Environment.NewLine;
+                    break;
+                case "what is the current time in Singapore":
+                    ss.SpeakAsync("current time in Singapore is " + getDateTime("Singapore").ToLongTimeString());
+                    txtContents.Text += getDateTime("Singapore").ToLongTimeString() + Environment.NewLine;
+                    break;
+                case "what is the current time in Honk kong":
+                    ss.SpeakAsync("current time in Honk kong is " + getDateTime("Honk kong").ToLongTimeString());
+                    txtContents.Text += getDateTime("Honk kong").ToLongTimeString() + Environment.NewLine;
+                    break;
+                case "what is the current time in Melbourne":
+                    ss.SpeakAsync("current time in Melbourne is " + getDateTime("Melbourne").ToLongTimeString());
+                    txtContents.Text += getDateTime("Melbourne").ToLongTimeString() + Environment.NewLine;
+                    break;
+                case "what is the current time in Tokyo":
+                    ss.SpeakAsync("current time in Tokyo is " + getDateTime("Tokyo").ToLongTimeString());
+                    txtContents.Text += getDateTime("Tokyo").ToLongTimeString() + Environment.NewLine;
+                    break;
+                case "what is the current time in Jakarta":
+                    ss.SpeakAsync("current time in Jakarta is " + getDateTime("Jakarta").ToLongTimeString());
+                    txtContents.Text += getDateTime("Jakarta").ToLongTimeString() + Environment.NewLine;
+                    break;
+
                 case "thank you":
                     ss.SpeakAsync("my pleasure");
                     break;
                 case "open chrome":
                     Process.Start("chrome", "https://www.youtube.com/watch?v=xug793JfqSE");
+
+                    break;
+                case "open notepad":
+                    Process.Start("notepad.exe");
 
                     break;
                 case "close":
@@ -150,6 +233,8 @@ namespace SpeechRecognitionApp {
 
 
         }
+
+
         private void Form1_Load(object sender, EventArgs e) {
 
         }
@@ -158,6 +243,14 @@ namespace SpeechRecognitionApp {
             sre.RecognizeAsyncStop();
             btnStart.Enabled = true;
             btnStop.Enabled = false;
+        }
+        public DateTime getDateTime(string cityName) {
+            int value = 0;
+            if (CitiesTimeZonesDicitonary.ContainsKey(cityName)) {
+                value = CitiesTimeZonesDicitonary[cityName];
+            }
+
+            return DateTime.Now.AddHours(value);
         }
 
         public static string GetLocalIPAddress() {
@@ -182,7 +275,7 @@ namespace SpeechRecognitionApp {
 
             return SystemInformation.PowerStatus.BatteryChargeStatus.ToString();
         }
-        private string getCurrentDate() {
+        public string getCurrentDate() {
             return DateTime.UtcNow.Date.ToString("dd/MM/yyyy");
         }
         public string ReturnCurrency(string URLstring, string filename, string searchKey, int index) {
@@ -191,9 +284,9 @@ namespace SpeechRecognitionApp {
 
                 using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
                 {
-                
+
                     client.DownloadFile(URLstring, filename);
-                
+
                     string line;
 
                     System.IO.StreamReader file =
@@ -202,16 +295,13 @@ namespace SpeechRecognitionApp {
                         bool found = line.Contains(searchKey);
                         if (found) {
                             returnValue = getCurrencyValue(line, index);
-                            //txtContents.Text += returnValue + Environment.NewLine;
 
-                            file.Close();
                             break;
                         }
                     }
                     file.Close();
                 }
-            }
-            catch (Exception ex) {
+            } catch (Exception ex) {
                 MessageBox.Show(ex.Message, "error");
             }
 
@@ -221,7 +311,7 @@ namespace SpeechRecognitionApp {
         public string getCurrencyValue(string myString, int index) {
             //120 for euro to smth
             //2 for bitcoin
-            char[] delimiterChars = { '>','<' };
+            char[] delimiterChars = { '>', '<' };
             string[] words = myString.Split(delimiterChars);
 
             return words[index];
